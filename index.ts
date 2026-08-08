153189a9bc41424090c5d3ff2ce01d05ec8b70b6
Mahadi Mokbul Ali <125756057+30mahadi@users.noreply.github.com>
2026-08-09 00:31:08 +0600

    Add files via upload

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-export { AddComponentDropdown } from "./AddComponentDropdown";
+export { default as McpManager } from "./manager";
+export { default as McpSidebar } from "./sidebar";
+export { default as McpDetail } from "./detail";
+export {
+  mcpAPI,
+  Tool,
+  CallToolResult,
+  ListToolsResponse,
+  CallToolResponse,
+} from "./api";
+
+export { default } from "./manager";
